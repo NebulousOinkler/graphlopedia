afcff0e9ff813341da78bac6ae0fc65fc12749f1
NebulousOinkler <sharatc@uw.edu>
2017-12-19 16:40:55 -0800

    MathSciNet to bibtex to JSON

--- htmltest.py
+++ htmltest.py
@@ -1,9 +1,15 @@
 from html.parser import HTMLParser
+import bibtexparser
+import re
+import json
 
-url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1=MR&s1=1019150"
-page = urllib.request.urlopen(url)
+#url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1=MR&s1=1019150"
+#page = urllib.request.urlopen(url)
+page = open('C:/Users/Sharat/Desktop/htmlTest.html')
 rawHTML = page.read()
 
-parser = HTMLParser()
-t = parser.feed(rawHTML)
-print(t)
+output  = re.compile('<pre>(.*?)</pre>', re.DOTALL |  re.IGNORECASE).findall(rawHTML)
+output = output[0]
+
+bib = bibtexparser.loads(output).entries[0]
+print(json.dumps(bib))
