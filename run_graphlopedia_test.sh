4e5d2793d9ae6e95eceeb31db3a1e0d726a200b1
NebulousOinkler <sharatc@uw.edu>
2018-01-07 20:44:33 -0800

    clone from master instead of old branch

--- run_graphlopedia_test.sh
+++ run_graphlopedia_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf graphlopedia_src
-git clone -b bibtexRefs https://github.com/NebulousOinkler/graphlopedia_src.git
+git clone https://github.com/NebulousOinkler/graphlopedia_src.git
 python -m unittest discover -s graphlopedia_src/test 2> output.log
